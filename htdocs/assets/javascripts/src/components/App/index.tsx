4d26024803a02339f24047affa50525f669e4e0d
YuNozato <mojahige@gmail.com>
2018-06-26 22:38:50 +0900

    Drawer を切り出して使用

--- htdocs/assets/javascripts/src/components/App/index.tsx
+++ htdocs/assets/javascripts/src/components/App/index.tsx
@@ -1,15 +1,16 @@
-import 'preact/devtools'
 import { h } from 'preact'
 import Router from 'preact-router'
 import Header from '../Header'
+import Drawer from '../Drawer'
 import Hello from '../Hello'
 // import AlertIcon from 'mdi-preact/AlertIcon'
 // import { Button } from 'preact-mdl'
 
 const App = () => (
-  <div id="layout">
+  <div id="layout" class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <Header />
-    <main>
+    <Drawer />
+    <main class="mdl-layout__content">
       <Router>
         <Hello path="/" />
       </Router>
---
+++ htdocs/assets/javascripts/src/components/Drawer/index.tsx
@@ -0,0 +1,24 @@
+import { h } from 'preact'
+
+const Drawer = () => (
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Title</span>
+    <nav class="mdl-navigation">
+      {/* <a class="mdl-navigation__link" href="">
+        Link
+      </a>
+      <a class="mdl-navigation__link" href="">
+        Link
+      </a>
+      <a class="mdl-navigation__link" href="">
+        Link
+      </a>
+      <a class="mdl-navigation__link" href="">
+        Link
+      </a> */}
+      underconstruction
+    </nav>
+  </div>
+)
+
+export default Drawer
