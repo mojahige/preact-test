b2114ae24a4a3a0ba140c4e0d4c5dca8f2a6932c
YuNozato <mojahige@gmail.com>
2018-06-26 22:39:36 +0900

    update Hello.

--- htdocs/assets/javascripts/src/components/Hello/index.tsx
+++ htdocs/assets/javascripts/src/components/Hello/index.tsx
@@ -2,31 +2,35 @@ import { h } from 'preact'
 
 const Hello = ({ path }) => (
   <section class="hello">
-    <h1>Hello!</h1>
-    <div>
-      <p>
-        My name is <em>mojahige</em> 😄
-      </p>
-      <p>
-        I do not understand English at all... 😅<br />
-        English may be strange.
-      </p>
-      <p>I am interested in Preact, I try various things 😏</p>
-      <p>This repository uses the following 👍</p>
+    <div class="hello__inner">
+      <h1>Hello!</h1>
+      <div>
+        <p>
+          My name is <em>mojahige</em> 😄
+        </p>
+        <p>
+          I do not understand English at all... 😅<br />
+          English may be strange.
+        </p>
+        <p>I am interested in Preact, I try various things 😏</p>
+        <p>This repository uses the following 👍</p>
 
-      <ul>
-        <li>
-          <a href="https://github.com/developit/preact">preact</a>
-        </li>
-        <li>
-          <a href="https://github.com/developit/preact-router">preact-router</a>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <a href="https://github.com/developit/preact">preact</a>
+          </li>
+          <li>
+            <a href="https://github.com/developit/preact-router">
+              preact-router
+            </a>
+          </li>
+        </ul>
 
-      <p>
-        There are also things that I can not use yet, so please see the
-        package.json for details.
-      </p>
+        <p>
+          There are also things that I can not use yet, so please see the
+          package.json for details.
+        </p>
+      </div>
     </div>
   </section>
 )
