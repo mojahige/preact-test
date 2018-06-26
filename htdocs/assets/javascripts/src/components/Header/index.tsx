3f3e735fdd21bb090837194ff2221f26125d54e1
YuNozato <mojahige@gmail.com>
2018-06-26 22:39:07 +0900

    ヘッダーのクラス名修正

--- htdocs/assets/javascripts/src/components/Header/index.tsx
+++ htdocs/assets/javascripts/src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import { h } from 'preact'
 
 const Header = () => (
-  <header class="Header">
-    <h1>test</h1>
+  <header class="Header mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <span class="mdl-layout-title">Title</span>
+    </div>
   </header>
 )
 
