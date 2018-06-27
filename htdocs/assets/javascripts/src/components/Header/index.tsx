550783a6b0269a2835690b04dcacfc3ce979ff40
YuNozato <mojahige@gmail.com>
2018-06-27 21:24:45 +0900

    不要クラス削除

--- htdocs/assets/javascripts/src/components/Header/index.tsx
+++ htdocs/assets/javascripts/src/components/Header/index.tsx
@@ -1,9 +1,9 @@
 import { h } from 'preact'
 
 const Header = () => (
-  <header class="Header mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <span class="mdl-layout-title">Title</span>
+  <header class="Header">
+    <div class="Header__inner">
+      <h1>Hello</h1>
     </div>
   </header>
 )
