6c4eefd66a0390ef74edbc75e776e66d5495d56e
Yu Nozato <nozato@j-yado.com>
2018-06-28 14:08:24 +0900

    Header でメニューを開く準備

--- htdocs/assets/javascripts/src/components/Header/index.tsx
+++ htdocs/assets/javascripts/src/components/Header/index.tsx
@@ -4,6 +4,12 @@ const Header = () => (
   <header class="Header">
     <div class="Header__inner">
       <h1>Hello</h1>
+
+      <button type="button" class="Header__drawerButton">
+        <i class="material-icons" aria-hidden="true">
+          menu
+        </i>
+      </button>
     </div>
   </header>
 )
