c661a1441d4f2385c00f9678df7b846a3661c638
YuNozato <mojahige@gmail.com>
2018-07-09 21:53:03 +0900

    マージミス
    SourceTree コマンドだけでやると Git は大変

--- htdocs/assets/javascripts/src/components/Profile/index.tsx
+++ htdocs/assets/javascripts/src/components/Profile/index.tsx
@@ -5,8 +5,6 @@ const Profile = ({ path, url, matches }: ComponentRouterProps) => (
   <section class="profile">
     <div class="profile__inner">
       <h1>Profile</h1>
-      <p>{path}</p>
-      <p>{url}</p>
     </div>
   </section>
 )
